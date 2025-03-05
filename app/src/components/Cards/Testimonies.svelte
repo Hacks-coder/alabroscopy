6178a497d6804f88cba276d223c6fa610d691c7d
Hacks-coder <ajheartcry@gmail.com>
2025-03-05 07:30:11 -0800

    fixed the testimonials sections

--- app/src/components/Cards/Testimonies.svelte
+++ app/src/components/Cards/Testimonies.svelte
@@ -2,32 +2,31 @@
 	import Small from "../Texts/Small.svelte";
 
     const testimonies = [
-        {
-            src: "/testimonies/img3.jpg",
-            title: "Bring your inner peace",
-            description: "This is the best cite to manage your health"
-        },
-        {
-            src: "/testimonies/img2.jpg",
-            title: "Come and manage with us",
-            description: "This is the best cite to manage your health"
-        },
-        {
-            src: "/testimonies/img1.jpg",
-            title: "Stabilise your heath with us",
-            description: "This is the best cite to manage your health"
-        }
-    ]
-    
+    {
+        src: "/testimonies/img3.jpg",
+        title: "A Healthier, Happier You",
+        description: "Managing my health has never been easier! This platform provides seamless access to medical services, ensuring I stay on top of my well-being."
+    },
+    {
+        src: "/testimonies/img2.jpg",
+        title: "Reliable and Life-Changing",
+        description: "This website has transformed the way I handle medical appointments and records. The efficiency and support are truly outstanding!"
+    },
+    {
+        src: "/testimonies/img1.jpg",
+        title: "Your Health, Our Priority",
+        description: "With this platform, I feel more in control of my health than ever before. The convenience and care it offers are simply unmatched!"
+    }
+]
     
 </script>
 
 
 <section>
     <div>
-        <div class="flex flex-col gap-10 @3xl:flex-row @5xl:gap-15">
+        <div class="flex flex-col items-center px-5 @md:px-10 @xl:px-15  gap-10 @3xl:px-5 @3xl:flex-row @3xl:px-20 @5xl:gap-15">
            {#each testimonies as {src, title, description}}
-                <div class="space-y-3">
+                <div class="flex flex-col items-center gap-2">
                     <div class="overflow-hidden w-60 rounded-2xl h-fit @md:w-80 @3xl:w-60 @4xl:w-80">
                         <img {src} alt="">
                     </div>
--- app/src/components/staticDirs/GetStarted.svelte
+++ app/src/components/staticDirs/GetStarted.svelte
@@ -9,8 +9,7 @@
         <div style={`background-image: url(${nurse})`} class="bg-cover bg-center size-72 @xl:size-80 @2xl:size-110 @4xl:size-150"></div>
         <div class="flex flex-col gap-5 items-center">
             <Title text = "How can we help you?" />
-            <Small text = "We collaborate with world-class experts to develop human-centered tools and services that drive sustainable growth. 
-                Our data-driven approach ensures that every decision is focused on enhancing your wellbeing." />
+            <Small text = "We collaborate with world-class experts to develop innovative, human-centered tools and services that drive sustainable growth and improve healthcare outcomes. Our data-driven approach ensures that every decision is carefully tailored to enhance your well-being, empowering you with smarter, more effective solutions. By integrating advanced technology with expert insights." />
         </div>
     </div>
 </section>
